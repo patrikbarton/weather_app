a281e11b6b34ee65c524fc02ca18972dfd0c58d2
pbarton <patrik.barton@bwsolution.de>
2024-08-09 11:54:57 +0200

    Add: Change Location -> Hamburg

--- frontend/angular-weather-app/src/app/weather.service.ts
+++ frontend/angular-weather-app/src/app/weather.service.ts
@@ -8,7 +8,7 @@ import {WeatherDto} from "./weather_api.interface";
 })
 export class WeatherService {
 
-  private apiUrl = 'http://api.weatherapi.com/v1/current.json?key=f09ff240632f45688da81422240908&q=London&aqi=no';
+  private apiUrl = 'http://api.weatherapi.com/v1/current.json?key=f09ff240632f45688da81422240908&q=Hamburg&aqi=no';
 
   constructor(private http: HttpClient) { }
 
